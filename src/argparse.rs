66a6561f427fdb10ad7669292f46619ef904235a
zhongqing0507 <18763884693@163.com>
2024-06-27 10:32:23 +0800

    add image enhance

--- src/argparse.rs
+++ src/argparse.rs
@@ -1,7 +1,7 @@
 use std::env;
 
 use anyhow::{Result, anyhow};
-use clap::{Parser, ValueEnum};
+use clap::{Parser,ValueEnum};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, ValueEnum, Serialize, Deserialize)]
@@ -46,6 +46,43 @@ impl BlendMode{
 }
 
 
+fn gamma_value_parser(s: &str) -> Result<f32, String> {
+    let value: f32 = s.parse().map_err(|_| format!("`{}` is not a valid number", s))?;
+    if value < 0.1 || value > 10.0 {
+        Err(format!("`{}` is out of range. It should be between 0.1 and 10", s))
+    } else {
+        Ok(value)
+    }
+}
+
+fn saturation_value_parser(s: &str) -> Result<f32, String> {
+    let value: f32 = s.parse().map_err(|_| format!("`{}` is not a valid number", s))?;
+    if value < -1.0 || value > 1.0 {
+        Err(format!("`{}` is out of range. It should be between -1.0 and 1.0", s))
+    } else {
+        Ok(value)
+    }
+}
+
+
+fn contrast_value_parser(s: &str) -> Result<f32, String> {
+    let value: f32 = s.parse().map_err(|_| format!("`{}` is not a valid number", s))?;
+    if value < -255.0 || value > 255.0 {
+        Err(format!("`{}` is out of range. It should be between -255.0 and 255.0", s))
+    } else {
+        Ok(value)
+    }
+}
+
+fn brightness_value_parser(s: &str) -> Result<f32, String>{
+    let value: f32 = s.parse().map_err(|_| format!("`{}` is not a valid number", s))?;
+    if value < -1.0 || value > 1.0 {
+        Err(format!("`{}` is out of range. It should be between -1.0 and 1.0", s))
+    } else {
+        Ok(value)
+    }
+}
+
 pub trait ArgParseProcess {
     type Error;
     fn output_folder(&self) -> Result<String>;
@@ -69,6 +106,24 @@ pub struct ArgParse{
     /// The blend mode, default is overlay
     #[arg(value_enum, short = 'm', long, default_value_t = BlendMode::Overlay)]
     pub blend_mode: BlendMode,
+
+    /// The gamma value, default is 1.0, range is [0.1, 10.0]
+    #[arg(short, long, value_parser = gamma_value_parser, default_value_t = 1.0)]
+    pub gamma: f32,
+
+    /// The saturation value, default is 0.0 ,range is [-1.0, 1.0]
+    #[arg(short, long, value_parser = saturation_value_parser, default_value_t = 0.0)]
+    pub saturation: f32,
+
+    /// The contrast value, default is 0.0, range is [-255.0, 255.0]
+    #[arg(short, long, value_parser = contrast_value_parser, default_value_t = 0.0)]
+    pub contrast: f32,
+
+    /// The brightness value, default is 0.0, range is [-1.0, 1.0]
+    #[arg(short, long, value_parser = brightness_value_parser, default_value_t = 0.0)]
+    pub brightness: f32,
+
+
 }
 
 
--- src/blend.rs
+++ src/blend.rs
@@ -1,5 +1,5 @@
 
-use photon_rs::{multiple::{self}, native, PhotonImage};
+use photon_rs::{colour_spaces, effects, multiple, native, PhotonImage};
 use crate::core::OUTPUT_FOLDER;
 use anyhow::{anyhow, Result};
 use crate::argparse::ArgParse;
@@ -9,15 +9,8 @@ impl BlendManager{
 
     pub fn open_image(path: &str) -> Result<PhotonImage> {
 
-        match photon_rs::native::open_image(path) {
-            Ok(img) => {
-                Ok(img)
-            },
-
-            Err(e) => {
-                return Err(anyhow!("image: {} open error: {}", path, e));
-            }
-        }
+        photon_rs::native::open_image(path)
+            .map_err(|e| anyhow!("image {} open error: {}", path, e))
     }
 
     pub fn blend_image_available(image: &PhotonImage, image2: &PhotonImage) -> bool {
@@ -30,15 +23,12 @@ impl BlendManager{
     }
 
     pub fn image_save(image: PhotonImage, blend_mode:&str) -> Result<String>{
-        let lock_output = OUTPUT_FOLDER.read().unwrap();
-        let output = match lock_output.clone() {
-            Some(output) => output,
-
-            None => {
-                return Err(anyhow!("No output folder specified"));
-            }
+        let output = OUTPUT_FOLDER.read().unwrap();
+        let output = if let Some(output) = output.as_ref(){
+            output
+        }else{
+            return Err(anyhow!("No output folder specified"));
         };
-
         let filename = format!("blend_{}.png", blend_mode);
         let save_path = Path::new(&output).join(filename);
         let save_path = save_path.to_str().unwrap();
@@ -53,7 +43,30 @@ impl BlendManager{
             }
         }
 
+    pub fn enhance<'a>(photon_image: &'a mut PhotonImage, options: &ArgParse) -> Result<&'a PhotonImage>{
+        let gamma = options.gamma;
+        let saturation = options.saturation;
+        let contrast = options.contrast;
+        let brightness = options.brightness;
 
+        colour_spaces::gamma_correction(photon_image, gamma, gamma, gamma);
+        if saturation >= 0.0{
+            colour_spaces::saturate_hsv(photon_image, saturation);
+        }else{
+            colour_spaces::desaturate_hsv(photon_image, saturation.abs());
+        }
+
+        effects::adjust_contrast(photon_image, contrast);
+
+        if brightness >= 0.0 {
+            colour_spaces::lighten_hsv(photon_image, brightness);
+        }else{
+            colour_spaces::darken_hsv(photon_image, brightness.abs());
+        }
+
+        Ok(photon_image)
+    }
+    
     pub fn blend(options: &ArgParse) -> Result<()>{
         let mut photon_image = Self::open_image(&options.image)?;
         let photon_image_2 = Self::open_image(&options.image2)?;
@@ -65,7 +78,8 @@ impl BlendManager{
         let blend_mode = options.blend_mode.blend_name();
         multiple::blend(&mut photon_image, &photon_image_2, &blend_mode);
 
-        let save_path = Self::image_save(photon_image, &blend_mode)?;
+        let photon_image = Self::enhance(&mut photon_image, &options)?;
+        let save_path = Self::image_save(photon_image.clone(), &blend_mode)?;
         println!("blend image save to {}", save_path);
         Ok(())
     }
